11cd84549c0b809f3a7b7d84d5cf9671e5ebe70b
Tom Forbes <tom@tomforb.es>
2020-07-25 00:13:31 +0000

    {"name":"autosys","version":"1.5.1","file":"autosys-1.5.1.tar.gz","path":"autosys/autosys-1.5.1.tar.gz"}

---
+++ autosys/autosys-1.5.1.tar.gz/AutoSys/utils/setup_utils/venv/bin/rst2s5.py
@@ -0,0 +1,23 @@
+#!/Users/skeptycal/Documents/coding/python/autosys/autosys/setup_utils/venv/bin/python3
+
+# $Id: rst2s5.py 4564 2006-05-21 20:44:42Z wiemann $
+# Author: Chris Liechti <cliechti@gmx.net>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing HTML slides using
+the S5 template system.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import default_description, publish_cmdline
+
+description = ('Generates S5 (X)HTML slideshow documents from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+publish_cmdline(writer_name='s5', description=description)
